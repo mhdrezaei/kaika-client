7220c8ed13ae856e0c9f2d3d78c10cf4351678ce
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-04-14 01:16:52 +0430

    complate Athentication

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,32 +1,58 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
-import { useMutation, useQueryClient } from "react-query";
-
-
-import { IauthLoginRequest, IauthLoginResponse } from "../types/api/api-types";
-import { loginUser } from "../service/api";
+import {
+  IauthLoginRequest,
+  IauthLoginResponse,
+  IuserInfo,
+} from "../types/api/api-types";
+import { getCurrentUser, loginUser } from "../service/api";
+import { AxiosResponse, AxiosResponseHeaders } from "axios";
 
 const useAuth = ({
-    onSuccess,
+  onSuccess,
 }: {
-    onSuccess?: () => void;
+  onSuccess?: () => void;
 } = {}) => {
-    const queryClient = useQueryClient();
-    const authMutation = useMutation(
-        async (authData: IauthLoginRequest) => {
-            const {data} = await loginUser(authData);
-            localStorage.setItem('token' , data.access_token)
-            return data;
-        },
-        {
-            onSuccess: async () => {
-                if (onSuccess) {
-                    onSuccess();
-                }
-            },
-        }
-    );
+  const queryClient = useQueryClient();
+  const authMutation = useMutation(
+    async (authData: IauthLoginRequest) => {
+      const { data, status } = await loginUser(authData);
+      console.log(status);
+      if (status === 200) {
+        localStorage.setItem(
+          "token",
+          data.access_token ? data.access_token : ""
+        );
 
-    return authMutation;
+        if (data.access_token) {
+          const { data, status } = await getCurrentUser();
+          console.log(status);
+          if (status === 200) {
+            localStorage.setItem("user", JSON.stringify(data));
+            queryClient.setQueryData("user", data);
+            return data;
+          } else {
+            console.log(data);
+            queryClient.setQueryData("loginError", data);
+            return data;
+          }
+        }
+      } else {
+        console.log(data);
+        return data;
+      }
+    },
+    {
+      onSuccess: async () => {
+        if (onSuccess) {
+          onSuccess();
+        }
+      },
+      onError(error: AxiosResponseHeaders, variables, context) {},
+    }
+  );
+
+  return authMutation;
 };
 
 export default useAuth;
--- src/page/Auth.tsx
+++ src/page/Auth.tsx
@@ -1,5 +1,4 @@
 import { useForm } from "react-hook-form";
-import { loginUser } from "../service/api";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler } from "react-hook-form/dist/types/form";
@@ -9,17 +8,24 @@ import {
   CardBody,
   CardFooter,
   Checkbox,
-  Button,
   Typography,
+  Alert,
 } from "@material-tailwind/react";
 import Input from "../components/common/Input";
 import { Link } from "react-router-dom";
 import { FiLock, FiMail } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../redux/store";
-import { useMutation } from "react-query/types/react";
+import { useQueryClient } from "react-query";
 import { IauthLoginRequest, IauthLoginResponse } from "../types/api/api-types";
-import useAuth from '../hooks/useAuth'
+import useAuth from "../hooks/useAuth";
+import { userAction } from "../redux/slice/user-slice";
+import SlideButton from "../components/common/SlideButton";
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/outline";
+
 const FormSchema = z.object({
   email: z.string().email("Please enter a valid email adress."),
   password: z
@@ -30,98 +36,130 @@ const FormSchema = z.object({
 
 type FormSchemaType = z.infer<typeof FormSchema>;
 
-
 const Auth = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const queryClient = useQueryClient();
 
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors, isSubmitting, isLoading },
   } = useForm<FormSchemaType>({
     resolver: zodResolver(FormSchema),
   });
+  console.log(isSubmitting);
   const authMutation = useAuth({
     onSuccess: () => {
-        console.log("success!!!!")
+      const user = queryClient.getQueryData("user");
+      dispatch(userAction.setUser(user));
     },
-});
-  const onSubmit: SubmitHandler<FormSchemaType> = (values:IauthLoginRequest) => {
-   authMutation.mutate(values)
-  } 
-  
-  
+  });
 
-   return (
+  const onSubmit: SubmitHandler<FormSchemaType> = (
+    values: IauthLoginRequest
+  ) => {
+    authMutation.mutate(values);
+  };
+
+  return (
     <>
-      <img
-        src="https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80"
-        className="absolute inset-0 z-0 h-full w-full object-cover"
-      />
-      <div className="absolute inset-0 z-0 h-full w-full  bg-kaika-black/50" />
-      <div className="container mx-auto p-4">
-        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4 bg-kaika-black">
-        <form
-          method="post"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <CardHeader
-            variant="gradient"
-            color="yellow"
-            className="mb-4 grid h-28 place-items-center bg-gradient-to-b from-kaika-yellow to-kaika-yellow-200 bg-kaika-yellow"
-          >
-            <Typography variant="h3" color="white">
-              Sign In
-            </Typography>
-          </CardHeader>
-          <CardBody className="flex flex-col gap-4">
-          
-          <Input
-            name="email"
-            label="Email address"
-            type="text"
-            icon={<FiMail />}
-            placeholder="example@emaple.com"
-            register={register}
-            error={errors?.email?.message}
-            disabled={isSubmitting}
-          />
+      <div className="absolute inset-0 z-0 h-full w-full  bg-kaika-gray" />
+      <div className="container mx-auto p-4 ">
+        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4 bg-kaika-black shadow-sm  shadow-kaika-yellow hover:shadow-md hover:shadow-kaika-yellow">
+          <form method="post" onSubmit={handleSubmit(onSubmit)}>
+            <CardHeader
+              variant="gradient"
+              className="mb-4 grid h-28 place-items-center  bg-kaika-yellow shadow-xl"
+            >
+              <Typography variant="h3" color="white">
+                Sign In
+              </Typography>
+            </CardHeader>
+            <CardBody className="flex flex-col gap-4">
+              <Input
+                name="email"
+                label="Email address"
+                type="text"
+                icon={<FiMail />}
+                placeholder="example@emaple.com"
+                register={register}
+                error={errors?.email?.message}
+                disabled={isSubmitting}
+              />
 
-          <Input
-            name="password"
-            label="Password"
-            type="password"
-            icon={<FiLock />}
-            placeholder="***********"
-            register={register}
-            error={errors?.password?.message}
-            disabled={isSubmitting}
-          />
-            <div className="-ml-2.5">
-              <Checkbox label="Remember Me" />
-            </div>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button type="submit" variant="gradient" fullWidth>
-              Sign In
-            </Button>
-            
-            <Typography variant="small" className="mt-6 flex justify-center">
-              Don't have an account?
-              <Link to="/auth/sign-up">
-                <Typography
-                  as="span"
-                  variant="small"
-                  color="blue"
-                  className="ml-1 font-bold"
-                >
-                  Sign up
-                </Typography>
-              </Link>
-            </Typography>
-          </CardFooter>
+              <Input
+                name="password"
+                label="Password"
+                type="password"
+                icon={<FiLock />}
+                placeholder="***********"
+                register={register}
+                error={errors?.password?.message}
+                disabled={isSubmitting}
+              />
+              <div className="-ml-2.5">
+                <Checkbox label="Remember Me" />
+              </div>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <SlideButton
+                type="submit"
+                text="Sign in"
+                slide_text="Secure sign in"
+                icon={<FiLock />}
+                disabled={authMutation.isLoading}
+              />
+
+              <Typography variant="small" className="mt-6 flex justify-center">
+                Forgot your password?
+                <Link to="/auth/sign-up">
+                  <Typography
+                    as="span"
+                    variant="small"
+                    color="blue"
+                    className="ml-1 font-bold"
+                  >
+                    Reset Password
+                  </Typography>
+                </Link>
+              </Typography>
+            </CardFooter>
           </form>
         </Card>
+      </div>
+      <div className="absolute bottom-5 w-full flex flex-col items-center justify-center mx-auto">
+        <Alert
+          show={authMutation.isSuccess}
+          color="green"
+          className="max-w-screen-md"
+          icon={<CheckCircleIcon className="mt-px h-6 w-6" />}
+          dismissible={{
+            onClose: () => console.log("close"),
+          }}
+        >
+          <Typography variant="h5" color="white">
+            Success
+          </Typography>
+          <Typography color="white" className="mt-2 font-normal">
+            singin was successfully. wellcome
+          </Typography>
+        </Alert>
+
+        <Alert
+          show={authMutation.isError}
+          color="red"
+          className="max-w-screen-md"
+          icon={<ExclamationTriangleIcon className="mt-px h-6 w-6" />}
+        >
+          <Typography variant="h5" color="white">
+            Failed
+          </Typography>
+          <Typography color="white" className="mt-2 font-normal">
+            {authMutation.isError
+              ? authMutation.error.response.data.message
+              : "somthing went wrong"}
+          </Typography>
+        </Alert>
       </div>
     </>
   );
